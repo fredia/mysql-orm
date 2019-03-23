a0e597aa7d2b8892b98fc8130311fc91183d4356
fredia <fredialee@163.com>
2019-03-23 23:31:15 +0800

    ADD:[] install mysql5.7 && test mysqlclient lib

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <mysql/mysql.h>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    MYSQL mysql;
+    mysql_init(&mysql);
+    cout << "mysql is running" << endl;
+    mysql_real_connect(&mysql, "localhost", "xxxx", "", "xxxx", 3306, NULL, 0);
+    string sql = " insert into student(id, name) values('12345', 'test');";
+    mysql_query(&mysql, sql.c_str());
+    mysql_close(&mysql);
     return 0;
 }
